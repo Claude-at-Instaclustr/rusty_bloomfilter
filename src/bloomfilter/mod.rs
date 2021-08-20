8db7ff6163a468f4ca55f6511ea85efb4ca4abb4
Claude Warren <claude.warren@instaclustr.com>
2021-08-20 13:44:19 +0100

    working compile

--- src/bloomfilter/mod.rs
+++ src/bloomfilter/mod.rs
@@ -3,79 +3,82 @@ use std::vec::Vec;
 
 type BloomFilterType = Box<dyn BloomFilter>;
 
-/// The traits that all BloomFilters must share 
+/// The traits that all BloomFilters must share
 pub trait BloomFilter {
 	/// Turn on the specified bit.
-	/// 
+	///
 	/// #Panics
 	/// Will panic if the selected bit is >=n `shape.m`
-	/// 
+	///
 	fn on( &mut self, bit : u32 );
-	
+
 	/// Turn off the specified bit.
-	/// 
+	///
 	/// #Panics
 	/// Will panic if the selected bit is >=n `shape.m`
-	/// 
+	///
 	fn off( &mut self, bit : u32 );
-	
+
 	/// return the filter as a vector of u32 bit vectors.
-	fn as_vec( self ) -> Box<Vec::<u32>>;
-	
+	fn as_vec( &self ) -> Box<&Vec::<u32>>;
+
 	/// return the shape of the filter
-	fn shape() -> Box<Shape>;
-	
+	fn shape( &self ) -> &Shape;
+
 	/// Determines if this filter contains the other filter.
-	/// 
+	///
 	/// #Errors
 	/// Returns an error if the shapes do not match.
-	/// 
+	///
 	fn contains( &self, other : BloomFilterType ) -> Result<bool, &str>
 	{
-		if self.shape().m != other.shape().m || self.shape.k != other.shape().k {
+		if self.shape().equivalent_to( other.shape() ) {
 			return Err( "Shapes do not match" )
-		} 
-		let selfBuff = self.as_vec();
-		let otherBuff = self.as_vec();
-		for i in 0..selfBuff.len() {
-			if selfBuff[i] & otherBuff[i] != otherBuff[i] {
+		}
+		let self_buff = self.as_vec();
+		let other_buff = self.as_vec();
+		for i in 0..self_buff.len() {
+			if self_buff[i] & other_buff[i] != other_buff[i] {
 				return Ok(false);
 			}
 		}
 		Ok(true)
 	}
-	
+
 	/// Gets the hamming value (the number of bits  turnd on).
 	fn hamming_value( &self ) -> u32;
-	
-	
+
+
 	/// Estimates the number of items in this filter
 	fn estimate_n( &self ) -> f32 {
-		return self.shape().estimate( self.hamming_value() );
+		return self.shape().estimate_n( self.hamming_value() );
 	}
-	
+
 	/// Estimates the number of items in the union of the two filters.
-	fn estimate_union( &self,  other : BloomFilterType ) -> f32 {
-		if self.shape().m != other.shape().m || self.shape.k != other.shape().k {
+	fn estimate_union( &self,  other : BloomFilterType ) -> Result<f32, &str> {
+		if self.shape().equivalent_to( other.shape() ) {
 			return Err( "Shapes do not match" )
-		} 
-		let selfBuff = self.as_vec();
-		let otherBuff = self.as_vec();
+		}
+		let self_buff = self.as_vec();
+		let other_buff = self.as_vec();
 		let mut count = 0;
-		for i in 0..selfBuff.len() {
-			let x = selfBuff[i] | otherBuff[i];
+		for i in 0..self_buff.len() {
+			let x = self_buff[i] | other_buff[i];
 			count += x.count_ones();
 		}
-		return self.shape().estimate( count );
+		return Ok( self.shape().estimate_n( count ) );
 	}
 
 	// estimate the number of items in the intersection of the two filters.
-	fn estimate_intersection( &self, other : BloomFilterType ) -> f32 {
-		return self.estimate_n() + other.estimate_n() - self.estimate_union( other );
+	fn estimate_intersection( &self, other : BloomFilterType ) -> Result<f32, &str> {
+		if self.shape().equivalent_to( other.shape() ) {
+			return Err( "Shapes do not match" )
+		}
+		return Ok(self.estimate_n() + other.estimate_n() - self.estimate_union( other ).unwrap());
 	}
 
 }
-    
+
 /// A prototype Bloom filer.
 ///
 /// Proto bloom filters can build filters of any shape.
@@ -84,13 +87,13 @@ pub trait Proto {
 	/// Create a bloom filter from the shape.
 	/// The specific type of the filter is implementation specific.
 	fn build( &self, shape : &Shape ) -> BloomFilterType;
-	
+
 	/// Add this proto to a filter.
 	fn add_to( &self,  filter : &mut BloomFilterType);
 }
 
 /// A Proto implementation that is a collection of Protos.
-/// 
+///
 /// This represents multiple objects to be placed in the filter.
 pub struct ProtoCollection {
 	inner: Vec<Box<dyn Proto>>,
@@ -117,19 +120,19 @@ impl SimpleProto {
 	pub fn new( full : u64 ) -> SimpleProto {
 		let start =   (full >> 32) as u32 ;
 		let incr = full  as u32;
-		
+
 		return SimpleProto{ start, incr };
 	}
 }
 
 impl Proto for SimpleProto {
 	/// Build a Simple Bloomfilter from the prototype
-	fn build( &self, shape : &Shape ) -> BloomFiltertype {
-		let mut filter : BloomFilter =  Simple::new( shape );
+	fn build( &self, shape : &Shape ) -> BloomFilterType {
+		let mut filter =  Simple::instance( shape );
 		self.add_to( &mut filter );
-		return Box.new(filter);
+		return filter;
 	}
-	
+
 	fn add_to( &self,  filter : &mut BloomFilterType ) {
 		let mut accumulator = self.start;
 		for _i in 0..filter.shape().k {
@@ -140,15 +143,16 @@ impl Proto for SimpleProto {
 }
 
 impl ProtoCollection {
+	/// Creates an empty collection
 	pub fn new() -> ProtoCollection {
 		return ProtoCollection{ inner : Vec::new() }
 	}
-	
+
 	/// Add prototypes to the collection
 	pub fn add(&mut self, proto : Box<dyn Proto> ) {
 		self.inner.push( proto );
 	}
-	
+
 	/// Gets the number of Protos in the collection
 	pub fn len(&self) -> usize {
 		return self.inner.len();
@@ -156,13 +160,14 @@ impl ProtoCollection {
 }
 
 impl Proto for ProtoCollection {
-	fn build( &self, shape : &Shape ) -> Simple {
-		let mut filter  =  Simple::new( shape );
+	/// create a Simple filter with the specified shape from this proto.
+	fn build( &self, shape : &Shape ) -> BloomFilterType {
+		let mut filter  =  Simple::instance( shape );
 		self.add_to( &mut filter );
 		return filter
 	}
-	
-	fn add_to( &self, filter : &mut BloomFilter) {
+
+	fn add_to( &self, filter : &mut BloomFilterType) {
 		for v in &self.inner {
 			v.add_to(  filter )
 		}
@@ -170,6 +175,10 @@ impl Proto for ProtoCollection {
 }
 
 impl Shape {
+
+	pub fn equivalent_to( &self, other : &Shape ) -> bool {
+		return self.m != other.m || self.k != other.k;
+	}
 	/// Calculaes the probability of false positives if `count` objects are added to a filter using this shape.
 	pub fn false_positives( &self, count : u32 ) -> f64 {
 		let k = self.k as f64;
@@ -177,17 +186,17 @@ impl Shape {
 		let m = self.m as f64;
 		return ( 1.0-((-k*n)/m).exp()).powf(k);
 	}
-	
+
 	/// Calculates the estimated number of items in a filter
 	/// of this Shape.
-	/// 
+	///
 	/// count is the number of bits turned on in the filter.
 	pub fn estimate_n( &self, count : u32 ) -> f32 {
 		let c = count as f64;
-		let m = self.shape.m as f64;
-		let k = self.shape.k as f64;
+		let m = self.m as f64;
+		let k = self.k as f64;
 		let result = -(m/k) * (1.0 - (c/m)).ln();
-		return result as f32; 
+		return result as f32;
 	}
 }
 
@@ -196,30 +205,33 @@ impl Shape {
 /// A bloom filter that stores the bits in an array of u32 bit buffers
 pub struct Simple {
 	shape : Shape,
-  	buffer : Vec::<u32>,	
+  	buffer : Vec::<u32>,
 }
 
 impl Simple {
 
-	/// Construct an empty Simple Bloom filter	
-	pub fn new( shape : &Shape ) -> BloomFilter {
-		
+	pub fn instance( shape : &Shape ) -> BloomFilterType {
+		return Box::new( Simple::new( shape ));
+	}
+
+	/// Construct an empty Simple Bloom filter
+	pub fn new( shape : &Shape ) -> Simple {
 		let size = if shape.m % 32 > 0  {1+(shape.m / 32)} else {shape.m / 32};
 		let buff = vec![0 as u32 ; size as usize ];
-		BloomFilter{ shape: shape.clone(), buffer : buff }
+		Simple{ shape: shape.clone(), buffer : buff }
 	}
-	
+
 }
 
 impl BloomFilter for Simple {
- 	fn as_vec(&self) -> &Vec::<u32> {
-		return &buffer;
+ 	fn as_vec(&self) -> Box<&Vec::<u32>> {
+		return Box::new( &self.buffer );
 	}
-	
+
 	fn shape(&self) -> &Shape {
-		reeturn &shape;
+		return &self.shape;
 	}
-	
+
 	fn on( &mut self, bit : u32 ) {
 		if bit >= self.shape.m {
 			panic!( "Bit position too large")
@@ -228,7 +240,7 @@ impl BloomFilter for Simple {
 		let position : usize = (bit / 32).try_into().unwrap();
 		self.buffer[position] |= 1<<offset;
 	}
-	
+
 	fn off( &mut self, bit : u32 ) {
 		if bit >= self.shape.m {
 			panic!( "Bit position too large")
@@ -237,43 +249,22 @@ impl BloomFilter for Simple {
 		let position : usize = (bit / 32).try_into().unwrap() ;
 		self.buffer[position] &= !(1<<offset);
 	}
-	
-	fn contains( &self, filter : &BloomFilter ) -> Result<bool, &str>
-	{
-		if self.shape.m != filter.shape.m || self.shape.k != filter.shape.k {
-			return Err( "Shapes do not match" )
-		} 
-		for i in 0..self.buffer.len() {
-			if self.buffer[i] & filter.buffer[i] != filter.buffer[i] {
-				return Ok(false);
-			}
-		}
-		Ok(true)
-	}
-	
+
 	fn hamming_value( &self ) -> u32 {
 		return self.buffer.iter().map(|i| i.count_ones() ).sum();
 	}
-	
-	fn estimate( &self, count : u32 ) -> f32 {
-		let c = count as f64;
-		let m = self.shape.m as f64;
-		let k = self.shape.k as f64;
-		let result = -(m/k) * (1.0 - (c/m)).ln();
-		return result as f32; 
-	}
-	
-	
+
+
 }
 //impl BloomFilter {
 //	pub fn new( shape : &Shape ) -> BloomFilter {
-//		
+//
 //		let size = if shape.m % 32 > 0  {1+(shape.m / 32)} else {shape.m / 32};
 //		let buff = vec![0 as u32 ; size as usize ];
 //		BloomFilter{ shape: shape.clone(), buffer : buff }
 //	}
-//	
-//	
+//
+//
 //	pub fn on( &mut self, bit : u32 ) {
 //		if bit >= self.shape.m {
 //			panic!( "Bit position too large")
@@ -282,7 +273,7 @@ impl BloomFilter for Simple {
 //		let position : usize = (bit / 32).try_into().unwrap();
 //		self.buffer[position] |= 1<<offset;
 //	}
-//	
+//
 //	pub fn off( &mut self, bit : u32 ) {
 //		if bit >= self.shape.m {
 //			panic!( "Bit position too large")
@@ -291,12 +282,12 @@ impl BloomFilter for Simple {
 //		let position : usize = (bit / 32).try_into().unwrap() ;
 //		self.buffer[position] &= !(1<<offset);
 //	}
-//	
+//
 //	pub fn contains( &self, filter : &BloomFilter ) -> Result<bool, &str>
 //	{
 //		if self.shape.m != filter.shape.m || self.shape.k != filter.shape.k {
 //			return Err( "Shapes do not match" )
-//		} 
+//		}
 //		for i in 0..self.buffer.len() {
 //			if self.buffer[i] & filter.buffer[i] != filter.buffer[i] {
 //				return Ok(false);
@@ -304,15 +295,15 @@ impl BloomFilter for Simple {
 //		}
 //		Ok(true)
 //	}
-//	
+//
 //	pub fn hamming_value( &self ) -> u32 {
 //		return self.buffer.iter().map(|i| i.count_ones() ).sum();
 //	}
-//	
+//
 //	pub fn estimate_n( &self ) -> f32 {
 //		return self.shape().estimate( self.hamming_value() );
 //	}
-//	
+//
 //	pub fn estimate_union( &self,  other : &BloomFilter ) -> f32 {
 //		let mut count = 0;
 //		for i in 0..self.buffer.len() {
@@ -322,7 +313,7 @@ impl BloomFilter for Simple {
 //		return self.estimate( count );
 //	}
 //
-//	
+//
 //	pub fn estimate_intersection( &self, other : &BloomFilter ) -> f32 {
 //		return self.estimate_n() + other.estimate_n() - self.estimate_union( other );
 //	}
@@ -331,13 +322,13 @@ impl BloomFilter for Simple {
 #[cfg(test)]
 mod tests {
 
-	
+
 	use crate::blooomfilter::Proto;
 
     #[test]
     fn shape_false_positives() {
 		let shape : super::Shape = super::Shape{ m : 134_191, n : 4000 , k : 23};
-		
+
         assert!(shape.false_positives()-(1.0/9_994_297.0) < 0.0000001 );
     }
 
@@ -364,7 +355,7 @@ mod tests {
 		assert!( bloomfilter.contains( &emptyFilter ).unwrap());
 		assert!( ! emptyFilter.contains( &bloomfilter ).unwrap());
 	}
-	
+
 	#[test]
 	fn shape_used_multiple_times() {
 		let shape : super::Shape = super::Shape{ m : 60, n : 4, k : 2 };
@@ -378,20 +369,20 @@ mod tests {
 		assert_eq!( bloomfilter2.buffer[0], 3 );
 		assert_eq!( bloomfilter2.buffer[1], 0 );
 	}
-	
+
 	#[test]
 	fn proto_collection() {
 		let shape : super::Shape = super::Shape{ m : 60, n : 4, k : 2 };
 		// this proto will turn on the left 'k' most bits
 		let proto : super::SimpleProto = super::SimpleProto::new( 1 );
-		// this proto will turn on the every other bit for a total of 'k' bits 
+		// this proto will turn on the every other bit for a total of 'k' bits
 		let proto2 = super::SimpleProto::new( 2 );
 		let mut collection = super::ProtoCollection::new();
 		collection.add( Box::new( proto ) );
 		collection.add( Box::new( proto2 ) );
 		assert_eq!( collection.len(), 2 );
 		let bloomfilter : super::BloomFilter = collection.build( &shape );
-		
+
 		assert_eq!( bloomfilter.buffer.len(), 2 );
 		assert_eq!( bloomfilter.buffer[0], 7 );
 		assert_eq!( bloomfilter.buffer[1], 0 );
@@ -399,8 +390,8 @@ mod tests {
 		//
 		// test collection containing a collection
 		//
-		
-		// this should yeild bits 0 and 16 (65536) 
+
+		// this should yeild bits 0 and 16 (65536)
 		let proto3 = super::SimpleProto::new( 0x100 );
 		let mut collection2 = super::ProtoCollection::new();
 		collection2.add( Box::new( collection ));
@@ -409,8 +400,8 @@ mod tests {
 		assert_eq!( bloomfilter2.buffer.len(), 2 );
 		assert_eq!( bloomfilter2.buffer[0], 7+65536 );
 		assert_eq!( bloomfilter2.buffer[1], 0 );
-		
-		
+
+
 	}
 
 }
